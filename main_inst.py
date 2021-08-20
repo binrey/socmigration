7aaba30d42118165f5b4f19913fb51bf24629bbf
Andrey <a.rybin@itelma.su>
2021-08-20 17:46:22 +0000

    working version

--- main_inst.py
+++ main_inst.py
@@ -6,18 +6,29 @@ from utils import mkdir
 import os
 
 
-def find_save_img(nimg):
+def find_imgs(src_list):
     art = soup.findChildren("article")[0]
-    img_out = None
     for img in art.findChildren("img"):
         if "data-testid" not in img.attrs.keys():
-            img_out = img
-            break
-    if img_out:
-        if "src" in img_out.attrs.keys():
-            impath = os.path.join(path2save, "img-{}.jpg".format(nimg))
-            urlretrieve(img_out.attrs["src"], impath)
-    return img_out
+            if "src" in img.attrs.keys():
+                if img.attrs["src"] not in src_list:
+                    src_list.append(img.attrs["src"])
+    return src_list
+
+
+def save_imgs(src_list):
+    for nimg, src in enumerate(src_list):
+        impath = os.path.join(path2save, "img-{}.jpg".format(nimg))
+        urlretrieve(src, impath)
+
+
+def find_videos():
+    art = soup.findChildren("article")[0]
+    if len(art.findChildren("video")):
+        video_link_path = os.path.join(path2save, "video-link.txt")
+        if not os.path.exists(video_link_path):
+            with open(video_link_path, "w") as f:
+                f.write("https://www.instagram.com/{}{}".format(parsed_user, plink))
 
 
 def find_nextbutton():
@@ -51,18 +62,22 @@ def find_time_and_story():
     for a in span.findChildren("a"):
         text += a.text + " "
     return time_stamp, text
+# ----------------------------------------------------------------------------------------------------
 
 
 working_login = "andrybin17"
-working_password = "vostok120461"
+working_password = "blabla"
 parsed_user = "andre.rybin"
-n_scroll = 25
+n_scroll = 22
 clear_folders = False
 
 mkdir("out", clear_dirs=clear_folders)
 mkdir("screens", clear_dirs=True)
 
-chromedriver = "/home/binrey/Dev/fb_parser/resourses/chromedriver"
+saved_posts = ["/p/{}/".format(d.split()[1]) for d in os.listdir("./out")]
+print("There are {} already parsed posts".format(len(saved_posts)))
+
+chromedriver = "resourses/chromedriver"
 options = webdriver.ChromeOptions()
 #options.add_argument('headless')  # для открытия headless-браузера
 browser = webdriver.Chrome(executable_path=chromedriver, options=options)
@@ -83,53 +98,56 @@ password.send_keys(working_password)
 
 submit = browser.find_element_by_tag_name('form')
 submit.submit()
-sleep(3)
+sleep(4)
 browser.save_screenshot("./screens/init{:03.0f}.png".format(1))
 
+browser.get("https://www.instagram.com/{}".format(parsed_user))
+sleep(3)
+posts_links = set([])
 for i in range(n_scroll):
-    browser.get("https://www.instagram.com/{}".format(parsed_user))
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(1)
+    sleep(2)
     browser.save_screenshot("./screens/scroll{:03.0f}.png".format(i))
-    print("{} scroll...".format(i))
+    print("{} scroll...".format(i), end=" ")
 
     soup = BeautifulSoup(browser.page_source, 'html5lib')
 
-    posts_links = []
     for div in soup.findChildren("div"):
         a = div.findChildren("a")
         if len(a):
             a = a[0].attrs["href"]
             if a.startswith("/p"):
-                posts_links += [a]
-    posts_links = set(posts_links)
+                if a not in saved_posts:
+                    posts_links.add(a)
 
-    for npost, plink in enumerate(posts_links):
-        browser.get("https://www.instagram.com/{}{}".format(parsed_user, plink))
-        sleep(1)
-        browser.save_screenshot("./screens/post{:03.0f}.png".format(npost))
+    print(len(posts_links), "new posts found")
+posts_links = list(posts_links)
 
-        soup = BeautifulSoup(browser.page_source, 'html5lib')
+print("\nstart parsing {} posts\n-------------------------------------------".format(len(posts_links)))
+for npost, plink in enumerate(posts_links):
+    print("post {:04.0f}/{:04.0f}".format(npost+1, len(posts_links)), end=" ")
+    browser.get("https://www.instagram.com/{}{}".format(parsed_user, plink))
+    sleep(1)
+    browser.save_screenshot("./screens/post{:03.0f}.png".format(npost))
 
-        time_stamp, story = find_time_and_story()
-        if time_stamp is None:
-            print("!!! broken post: {}".format(plink))
-            continue
+    soup = BeautifulSoup(browser.page_source, 'html5lib')
 
-        path2save = os.path.join("./out", "{}".format(time_stamp))
-        if mkdir(path2save, clear_dirs=clear_folders):
-            with open(os.path.join(path2save, "story.txt"), "w") as f:
-                f.write(story)
+    time_stamp, story = find_time_and_story()
+    if time_stamp is None:
+        print("!!! broken post: {}".format(plink))
+        continue
 
-            nimg = 0
-            find_save_img(nimg)
+    path2save = os.path.join("./out", "{} {}".format(time_stamp, plink.replace("/p/", "").replace("/", "")))
+    if mkdir(path2save, clear_dirs=clear_folders):
+        with open(os.path.join(path2save, "story.txt"), "w") as f:
+            f.write(story)
+
+        src_list = find_imgs([])
+        next_button = find_nextbutton()
+        while next_button is not None:
+            browser.find_elements_by_class_name(next_button.attrs["class"][0])[0].click()
+            soup = BeautifulSoup(browser.page_source, 'html5lib')
+            src_list = find_imgs(src_list)
             next_button = find_nextbutton()
-
-            while next_button is not None:
-                nimg += 1
-                browser.find_elements_by_class_name(next_button.attrs["class"][0])[0].click()
-                soup = BeautifulSoup(browser.page_source, 'html5lib')
-                find_save_img(nimg)
-                next_button = find_nextbutton()
-
-
+        save_imgs(src_list)
+        find_videos()
